fd07f44df4fb3846958ef1e742bfc5412eca73ae
carlosAlexandre <142914111+carlosAlexandre7@users.noreply.github.com>
2024-05-09 09:29:38 -0300

    Update SQLQuery3 (exercício de SELECT).sql

--- bdEscolaDeIdiomas/SQLQuery3 (exercício de SELECT).sql
+++ bdEscolaDeIdiomas/SQLQuery3 (exercício de SELECT).sql
@@ -28,40 +28,39 @@ SELECT COUNT(tbMatricula.codAluno), tbMatricula.dataMatricula FROM tbAluno
 	WHERE MONTH(dataMatricula) = 5 AND YEAR(dataMatricula) = 2016
 	GROUP BY tbMatricula.dataMatricula;
 
---Apresentar o nome dos alunos em ordem alfabÈtica ao lado do nome das turmas e os nomes dos cursos em que est„o matriculados--
+--Apresentar o nome dos alunos em ordem alfab√©tica ao lado do nome das turmas e os nomes dos cursos em que est√£o matriculados--
 
 SELECT nomeAluno, nomeTurma FROM tbAluno
 	INNER JOIN tbMatricula ON tbMatricula.codAluno = tbAluno.codAluno
 		INNER JOIN tbTurma ON tbMatricula.codTurma = tbTurma.codTurma
 		ORDER BY nomeAluno
 
---Apresentar o nome dos cursos e os hor·rios em que eles s„o oferecidos--
+--Apresentar o nome dos cursos e os hor√°rios em que eles s√£o oferecidos--
 
 SELECT nomeCurso, tbTurma.horarioTurma FROM tbTurma
 	INNER JOIN tbCurso ON tbTurma.codCurso = tbCurso.codCurso
 
 --Apresentar a quantidade de alunos nascidos por estado que estejam matriculados no curso de ingles--
 
-SELECT COUNT(tbAluno.codAluno), naturalidadeAluno, nomeCurso FROM tbAluno
+SELECT COUNT(tbAluno.nomeAluno), naturalidadeAluno, tbTurma.codCurso FROM tbAluno
 	INNER JOIN tbMatricula ON tbMatricula.codAluno = tbAluno.codAluno
 		INNER JOIN tbTurma ON tbMatricula.codTurma = tbTurma.codTurma
-			INNER JOIN tbCurso ON tbTurma.codCurso = tbCurso.codCurso
-			WHERE nomeCurso LIKE 'InglÍs'
-			GROUP BY naturalidadeAluno
+			WHERE (tbTurma.codCurso) = 1
+			GROUP BY tbTurma.codcurso, naturalidadeAluno
 
---Apresentar o nome dos alunos ao lado da data de matrÌcula no formato dd/mm/aaaa--
+--Apresentar o nome dos alunos ao lado da data de matr√≠cula no formato dd/mm/aaaa--
 
 SELECT nomeAluno, dataMatricula FROM tbAluno
 	INNER JOIN tbMatricula ON tbMatricula.codAluno = tbAluno.codAluno
 
---Apresentar os alunos cujo nome comece com A e que estejam matriculados no curso de inglÍs--
+--Apresentar os alunos cujo nome comece com A e que estejam matriculados no curso de ingl√™s--
 
 SELECT nomeAluno, nomeCurso FROM tbAluno
 	INNER JOIN tbMatricula ON tbMatricula.codAluno = tbAluno.codAluno
 		INNER JOIN tbTurma ON tbMatricula.codTurma = tbTurma.codTurma
 			INNER JOIN tbCurso ON tbTurma.codCurso = tbCurso.codCurso
 			WHERE nomeAluno LIKE 'a%' 
-			AND nomeCurso LIKE 'Ingles' 
+			AND nomeCurso LIKE 'Ingl√™s' 
 
 --Apresentar a quantidade de matriculas feitas no ano de 2016--
 
@@ -82,9 +81,9 @@ SELECT COUNT(tbMatricula.codAluno) FROM tbCurso
  INNER JOIN tbMatricula ON tbTurma.codTurma = tbMatricula.codTurma
 WHERE tbCurso.valorCurso > 300.00;
 
---Apresentar os nomes dos alunos que fazem o curso de alem„o--
+--Apresentar os nomes dos alunos que fazem o curso de alem√£o--
 SELECT tbCurso.nomeCurso, tbAluno.nomeAluno FROM tbAluno
 INNER JOIN tbMatricula ON tbAluno.codAluno = tbMatricula.codAluno
 INNER JOIN tbTurma ON tbMatricula.codTurma = tbTurma.codTurma
 INNER JOIN tbCurso ON tbTurma.codCurso = tbCurso.codCurso
-WHERE tbCurso.nomeCurso = 'Alem„o';
+WHERE tbCurso.nomeCurso = 'Alem√£o';
