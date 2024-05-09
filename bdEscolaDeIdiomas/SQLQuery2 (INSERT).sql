0b25db8998d6711549301773c6b9382aae84e946
carlosAlexandre <142914111+carlosAlexandre7@users.noreply.github.com>
2024-05-09 09:28:48 -0300

    Update SQLQuery2 (INSERT).sql

--- bdEscolaDeIdiomas/SQLQuery2 (INSERT).sql
+++ bdEscolaDeIdiomas/SQLQuery2 (INSERT).sql
@@ -1,26 +1,36 @@
-USE bdEscolaIdiomas
+INSERT INTO tbAluno
+  (nomeAluno, dataNascAluno, rgAluno, naturalidadeAluno)
+    VALUES
+	 ('Paulo Santos', '2000-03-10', 822821220, 'SP')
+	 ,('Maria da Gloria', '1999-10-03', 822821220, 'SP')
+	 ,('Pedro Noqueira da Silva', '1998-04-04', 822821220, 'SP')
+	 ,('Gilsonn Barros Silva', '1995-09-09', 822821220, 'PE')
+	 ,('Mariana Barbosa Santos', '2001-10-10', 822821220, 'RJ')
+	 ,('Alessandro Pereira', '2003-10-11', 822821220, 'ES')
+	 ,('Aline Melo', '2001-10-08', 822821220, 'RJ')
 
--- Inserir dados na tabela tbCurso
-INSERT INTO tbCurso (nomeCurso, cargahorariaCurso, valorCurso) VALUES
-('Inglês', 2000, 356.00),
-('Alemão', 3000, 478.00),
-('Espanhol', 4000, 500.00);
+INSERT INTO tbCurso
+   (nomeCurso, cargaHorariaCurso, valorCurso)
+   VALUES
+    ('InglÃªs', 2000, 356.00)
+	,('AlemÃ£o', 3000, 478.00)
+	,('Espanhol', 4000, 500.00)
 
--- Inserir dados na tabela tbTurma
-INSERT INTO tbTurma (nomeTurma, codCurso, horarioTurma) VALUES
-('1|A', 1, '1900-01-01 12:00'),
-('1|C', 1, '1900-01-01 18:00'),
-('1|B', 1, '1900-01-01 18:00'),
-('1AA', 1, '1900-01-01 19:00');
+INSERT INTO tbTurma
+  (nomeTurma, horarioTurma, codCurso)
+  VALUES
+   ('1|A', '1900-01-01 12:00:00', 1)
+   ,('1|B', '1900-01-01 18:00:00', 3)
+   ,('1|C', '1900-01-01 18:00:00', 1)
+   ,('1|AA', '1900-01-01 19:00:00', 2)
 
--- Inserir dados na tabela tbAluno
-INSERT INTO tbAluno (nomeAluno, dataNascAluno, rgAluno, naturalidadeAluno) VALUES
-('Paulo Santos', '2000-03-10', '82.282.122-0', 'SP'),
-('Maria da Gloria', '1999-10-03', '45.233.123-0', 'SP'),
-('Pedro Nogueira da Silva', '1998-04-04', '23.533.211-9', 'SP');
-
--- Inserir dados na tabela tbMatricula
-INSERT INTO tbMatricula (dataMatricula, codAluno, codTurma) VALUES
-('2024-04-25', 1, 1),
-('2024-04-26', 2, 2),
-('2024-04-27', 3, 3);
+INSERT INTO tbMatricula
+   (dataMatricula, codAluno, codTurma)
+   VALUES
+     ('2015-10-03', 1, 1)
+	 ,('2014-05-04', 2, 1)
+	 ,('2014-05-04', 2, 4)
+	 ,('2012-05-03', 3, 2)
+	 ,('2016-03-03', 1, 3)
+	 ,('2015-05-07', 4, 2)
+	 ,('2015-07-05', 4, 3)
