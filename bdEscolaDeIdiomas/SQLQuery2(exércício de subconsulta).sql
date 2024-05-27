717760396d13745e8a8cb2e72a3ca844c3332da0
carlosAlexandre <142914111+carlosAlexandre7@users.noreply.github.com>
2024-05-26 21:18:03 -0300

    Add files via upload

---
+++ bdEscolaDeIdiomas/SQLQuery2(exércício de subconsulta).sql
@@ -0,0 +1,10 @@
+--Criar uma consulta que retorne o nome e o preço dos cursos que custem abaixo do valor médio
+
+SELECT nomeCurso, valorCurso FROM tbCurso
+WHERE valorCurso >= (SELECT AVG (valorCurso) FROM tbCurso)
+
+--Criar uma consulta que retorne o nome e o rg do aluno mais novoSELECT nomeAluno, rgAluno FROM tbAlunoWHERE YEAR(dataNascAluno) = (SELECT MIN(YEAR(dataNascAluno)) FROM tbAluno)
+--Criar uma consulta que retorne o nome do aluno mais velho
+SELECT nomeAluno FROM tbAlunoWHERE YEAR(dataNascAluno) = (SELECT MAX(YEAR(dataNascAluno)) FROM tbAluno)
+
+--Criar uma consulta que retorne o nome e o valor do curso mais caroSELECT nomeCurso, valorCurso FROM tbCursoWHERE valorCurso = (SELECT MAX(valorCurso) FROM tbCurso)--Criar uma consulta que retorne o nome do aluno e o nome do curso, do aluno que fez a última matrículaSELECT nomeAluno, nomeCurso FROM tbAlunoINNER JOIN tbMatricula ON tbMatricula.codAluno = tbAluno.codAlunoINNER JOIN tbTurma ON tbTurma.codTurma = tbAluno.codAlunoINNER JOIN tbCurso ON tbCurso.codCurso = tbTurma.codCurso	WHERE dataMatricula = (SELECT MAX(dataMatricula) FROM tbMatricula) --Criar uma consulta que retorne o nome do primeiro aluno a ser matriculado na escola de IdiomasSELECT nomeAluno FROM tbAlunoINNER JOIN tbMatricula ON tbMatricula.codAluno = tbAluno.codAlunoWHERE dataMatricula = (SELECT MIN(dataMatricula) FROM tbMatricula) --Criar uma consulta que retorne o nome, rg e data de nascimento de todos os alunos que estejam matriculados no curso de inglêsSELECT nomeAluno, rgAluno, dataNascAluno FROM tbAlunoINNER JOIN tbMatricula ON tbMatricula.codAluno = tbAluno.codAlunoINNER JOIN tbTurma ON tbTurma.codTurma = tbAluno.codAlunoINNER JOIN tbCurso ON tbCurso.codCurso = tbTurma.codCurso	WHERE nomeCurso = 'inglês'
